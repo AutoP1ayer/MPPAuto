50bf1c8cbf8e03a932736203099cafea5ba76323
AutoPlayer <100078941+AutoP1ayer@users.noreply.github.com>
2022-03-08 11:49:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 $(function () {
 
   console.log("%cWelcome to MPP's developer console!", "color:blue; font-size:20px;");
-  console.log("%cCheck out the source code: https://github.com/LapisHusky/mppclone/tree/main/client\nGuide for coders and bot developers: https://docs.google.com/document/d/1OrxwdLD1l1TE8iau6ToETVmnLuLXyGBhA0VfAY1Lf14/edit?usp=sharing", "color:gray; font-size:12px;")
+  console.log("%cCheck out the source code: https://github.com/AutoP1ayer/MPPAuto\nGuide for coders and bot developers: https://docs.google.com/document/d/1FvHDNN5C5dbY6M898ouQ50ix4HDodibmLboaVBIX9PA/edit?usp=sharing", "color:gray; font-size:12px;")
 
 
 
